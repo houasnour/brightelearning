bbef790e4c0498fc745d6291f07b84cb220a1c67
houasnour <houasnour11@gmail.com>
2026-02-25 02:13:16 +0100

    setting up the project and structure and backend sever

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express= require('express');
+const dotenv=require('dotenv');
+const cors=require('cors');
+const app=express();
+
+dotenv.config();
+//middelware
+app.use(cors());
+app.use(express.json());
+
+//connectin data base 
+
+
+
+//starting the server
+const PORT=process.env.PORT|| 5000;
+app.listen(PORT,()=>{
+    console.log(`server started at port${PORT}`);
+
+});
